d3bf0ef19cc7a15ab54b38e19e432bef05e901ed
unknown <mohammad.hm.836@gmail.com>
2026-07-17 11:16:24 +0300

    Create authentication architecture structure

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\V1\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::prefix('v1')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
